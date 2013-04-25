202b09d4e1d4958d091d6b275c3e81c0f37657ab
Andreas <Andreas@chase.andhen.mine.nu>
2013-04-25 23:16:20 +0200

    Implemented lots of rules in the parser.

--- rd_parser.py
+++ rd_parser.py
@@ -7,14 +7,26 @@ class Parser:
         self.tokens = deque(tokens)
         self.current = None
         self.matched_rule = None
+        self.discarded = deque()
     def next(self):
-        self.previous = self.current
+        self.discarded.append(self.current)
         self.current = self.tokens.popleft()
-        print("Current token: "+self.current.type)
     def back(self):
         self.tokens.appendleft(self.current)
-        self.current = self.previous
-    
+        self.current = self.discarded.pop()
+        
+    def accept_multiple_tokens(self,tokens):
+        matched = []
+        for token in tokens:
+            if len(token) == 1:
+                token.append(None)
+            if self.accept(token[0], token[1]):
+                matched.append(self.current)
+            else:
+                for match in matched: self.back()
+                return None
+        return matched
+
     def accept(self,token_type,value=None):
         self.next()
         if self.current.type != token_type:
@@ -27,38 +39,106 @@ class Parser:
     def expect(self,token_type,value=None):
         self.next()
         if self.current.type != token_type:
-            raise Exception("Expected token of type: "+token_type+" was: "+self.current.type)
+            raise Exception("Expected "+token_type+" but found "+self.current.type)
         if value != None and self.current.value != value:
-            raise Exception("Expected token of type: "+token_type+" and value: "+str(value))
+            raise Exception("Expected: "+token_type+" with value: "+str(value))
         return self.current
     def acceptRule(self,rule_class):
         self.matched_rule = rule_class.match(self)
         return self.matched_rule
+    
+    def acceptAnyRule(self,rules):
+        for rule in rules:
+            if self.acceptRule(rule):
+                return self.matched_rule
+        return None
+    
     def expectRule(self,rule_class):
         self.matched_rule = rule_class.match(self)
         if not self.matched_rule:
-            raise Exception("Expected :"+type(rule_class))
+            raise Exception("Expected "+rule_class.__name__)
+        return self.matched_rule
     def expectNewline(self):
-        if self.accept(Token.EOF):
-            return None
         self.expect(Token.NEWLINE)
+    def isNextEOF(self):
+        self.next()
+        result = self.current.type == Token.EOF
+        self.back()
+        return result
         
 
 class Rule:
+    def __init__(self,nodes):
+        self.nodes = nodes
     def __repr__(self):
-        return "%s(%r)" % (self.__class__, self.__dict__)
+        return "%s(%r)" % (self.__class__.__name__, self.nodes)
+    
+class Declaration(Rule):
+    @staticmethod
+    def match(parser): 
+        if not (parser.accept(Token.KEYWORD,KeywordToken.DIM) or 
+                parser.accept(Token.KEYWORD,KeywordToken.LOCAL) or
+                parser.accept(Token.KEYWORD,KeywordToken.GLOBAL)):
+            return None
+        nodes = [parser.current]
+        nodes.append(parser.expectRule(SmallStatement))
+        if parser.acceptRule(Assignment):  
+            nodes.append(parser.matched_rule)
+        return Declaration(nodes)
+    
+
+class Assignment(Rule):
+    operators = ["=","+=","-=","*=","/=","&="]
+    @staticmethod
+    def match(parser):
+        for operator in Assignment.operators:
+            if parser.accept(Token.OPERATOR,operator):
+                nodes = [parser.current]
+                nodes.append(parser.expectRule(Expression))
+                return Assignment(nodes)
+        return None
+ 
+class SmallStatement(Rule):
+    @staticmethod
+    def match(parser):
+        if not parser.accept(Token.IDENTIFIER):
+            return None
+        nodes = [parser.current]
+        while parser.acceptAnyRule([ListIndexing,Property]):
+            nodes.append(parser.matched_rule)
+        return SmallStatement(nodes)
+
+class Statement(Rule):
+    @staticmethod
+    def match(parser):
+        if parser.acceptAnyRule([Declaration,Function,While,If]):
+            return Statement([parser.matched_rule])
+        if parser.acceptRule(SmallStatement):
+            nodes = [parser.matched_rule]
+            if parser.acceptRule(Assignment):
+                nodes.append(parser.matched_rule)
+                return Statement(nodes)
+            if parser.acceptRule(Call):
+                nodes.append(parser.matched_rule)
+            if parser.acceptRule(BinaryOperator):
+                nodes.append(parser.matched_rule)
+                nodes.append(parser.expectRule(Expression))
+            return Statement(nodes)
+        if parser.acceptRule(Expression):
+            return Statement([parser.matched_rule])
+        
+        return None
+
+            
 
 class Function(Rule):
-    def __init__(self,name,arguments,body):
-        self.name = name
-        self.arguments = arguments
-        self.body = body
     @staticmethod
     def match(parser):
         if parser.accept(Token.KEYWORD,KeywordToken.FUNC):
             func_name = parser.expect(Token.IDENTIFIER)
             parser.expect(Token.LEFT_PAREN)
             arguments = []
+            body = None
             while parser.accept(Token.IDENTIFIER):
                 arguments.append(parser.current)
                 if not parser.accept(Token.COMMA):
@@ -66,26 +146,93 @@ class Function(Rule):
             parser.expect(Token.RIGHT_PAREN)
             parser.expectNewline() 
             
-            # Match a function body here
+            body = parser.acceptRule(Block)
             
             parser.expect(Token.KEYWORD,KeywordToken.ENDFUNC)
-            parser.expectNewline()
             
-            return Function(func_name,arguments,None)
+            return Function([func_name,arguments,body])
         else:
             return None
         
 class Block(Rule):
-    def __init__(self,blocks):
-        self.blocks = blocks
     @staticmethod
     def match(parser):
-        pass
+        nodes = []
+        something_matched = False
+        while True:
+            if parser.accept(Token.NEWLINE):
+                something_matched = True
+                continue
+            if parser.acceptRule(Statement):
+                something_matched = True
+                nodes.append(parser.matched_rule)
+                if not parser.isNextEOF():
+                    parser.expect(Token.NEWLINE)
+                continue
+            break
+        if something_matched: return Block(nodes)
+        return None
+        
+
+class While(Rule):
+    @staticmethod
+    def match(parser):
+        if not parser.accept(Token.KEYWORD,KeywordToken.WHILE):
+            return None
+        nodes = []
+        nodes.append(parser.expectRule(Expression))
+        if parser.acceptRule(Block):
+            nodes.append(parser.matched_rule)
+        parser.expect(Token.KEYWORD,KeywordToken.WEND)
+        return While(nodes)
+#<if> = "if", <expression>, "then", ("newline", [<block>], [{<elseif>}], [<else>], "endif")|<statement>
+#<elseif> = "elseif", <expression>, "then", "newline",[<block>]
+#<else> = "else", "newline", <block>
+class If(Rule):
+    @staticmethod
+    def match(parser):
+        if not parser.accept(Token.KEYWORD,KeywordToken.IF):
+            return None
+        nodes = []
+        nodes.append(parser.expectRule(Expression))
+        parser.expect(Token.KEYWORD,KeywordToken.THEN)
+        if parser.accept(Token.NEWLINE):
+            if parser.acceptRule(Block):
+                nodes.append(parser.matched_rule)
+            while parser.acceptRule(ElseIf):
+                nodes.append(parser.matched_rule)
+            if parser.acceptRule(Else):
+                nodes.append(parser.matched_rule)
+            parser.expect(Token.KEYWORD,KeywordToken.ENDIF)
+        else:
+            # TODO: Only allow single line statement
+            nodes.append(parser.expectRule(Statement))    
+        return If(nodes)
+
+class ElseIf(Rule):
+    @staticmethod
+    def match(parser):
+        if not parser.accept(Token.KEYWORD,KeywordToken.ELSEIF):
+            return None 
+        nodes = [parser.expectRule(Expression)]
+        parser.expect(Token.KEYWORD,KeywordToken.THEN)
+        parser.expect(Token.NEWLINE)
+        if parser.acceptRule(Block):
+            nodes.append(parser.matched_rule)
+        return ElseIf(nodes)
+
+class Else(Rule):
+    @staticmethod
+    def match(parser):
+        if not parser.accept(Token.KEYWORD,KeywordToken.ELSE):
+            return None 
+        nodes = []
+        parser.expect(Token.NEWLINE)
+        if parser.acceptRule(Block):
+            nodes.append(parser.matched_rule)
+        return Else(nodes)
 
 class Directive(Rule):
-    def __init__(self,directive_token,argument=None):
-        self.argument = argument
-        self.directive_token = directive_token
     @staticmethod
     def match(parser):
         argument = None
@@ -95,49 +242,162 @@ class Directive(Rule):
             directive = parser.current
             argument = parser.expect(Token.STRING)
             parser.expectNewline()
-            return Directive(directive,argument)
+            return Directive([directive,argument])
         if parser.accept(Token.DIRECTIVE):
             token = parser.current
             parser.expectNewline()
-            return Directive(token)
+            return Directive([token])
         
         return None
     
 class Include(Rule):
-    def __init__(self,include_token):
-        self.include_token = include_token
-        
     @staticmethod
     def match(parser):
         if parser.accept(Token.INCLUDE_FILE):
             token = parser.current
             parser.expectNewline()
-            return Include(token)
-    
+            return Include([token])
         
-
-class Program(Rule):
-    def __init__(self,directives,block):
-        self.directives = directives
-        self.block = block
+        
+class UnaryOperator(Rule):
     @staticmethod
     def match(parser):
-        directives = []
-        while parser.acceptRule(Include) or parser.acceptRule(Directive):
-            directives.append(parser.matched_rule)
-        return Program(directives,parser.acceptRule(Block))
+        if parser.accept(Token.OPERATOR,"not") or parser.accept(Token.OPERATOR,"-"):
+            return UnaryOperator([parser.current])
+
+
+
+class BinaryOperator(Rule):
+    operators = [">=","<=","<>","<",">","-","=","*","+","-","/","&","==","^","and","or"]
+    @staticmethod
+    def match(parser):
+        for operator in BinaryOperator.operators:
+            if parser.accept(Token.OPERATOR,operator):
+                return BinaryOperator([parser.current])
+        return None
+
+class Factor(Rule):
+    @staticmethod
+    def match(parser):
+        nodes = []
+        if parser.acceptAnyRule([Terminal,InlineList]):
+            nodes.append(parser.matched_rule)
+        elif parser.accept(Token.LEFT_PAREN):
+            nodes.append(parser.expectRule(Expression))
+            parser.expect(Token.RIGHT_PAREN)
+        else:
+            return None
+        while parser.acceptAnyRule([Call,ListIndexing,Property]):
+            nodes.append(parser.matched_rule)
+        return Factor(nodes)
+
+                
+                
         
+class InlineList(Rule):
+    @staticmethod
+    def match(parser):
+        if not parser.accept(Token.LEFT_BRACKET):
+            return None
+        nodes = []
+        if parser.acceptRule(Expression):
+            nodes.append(parser.matched_rule)
+            while parser.accept(Token.COMMA):
+                nodes.append(parser.expectRule(Expression))
+
+        parser.expect(Token.RIGHT_BRACKET)
+        return InlineList(nodes)
         
-    
-    
             
+
+class Call(Rule):
+    @staticmethod
+    def match(parser):
+        if not parser.accept(Token.LEFT_PAREN):
+            return None
+        nodes = []
+        if parser.acceptRule(Expression):
+            nodes.append(parser.matched_rule)
+            while parser.accept(Token.COMMA):
+                nodes.append(parser.expectRule(Expression))
+        parser.expect(Token.RIGHT_PAREN)
+        return Call(nodes)
+        
+
+class Property(Rule):
+    @staticmethod
+    def match(parser):
+        if not parser.accept(Token.DOT):
+            return None
+        return Property([parser.expect(Token.IDENTIFIER)])
+
+
+class ListIndexing(Rule):
+    @staticmethod
+    def match(parser):
+        if not parser.accept(Token.LEFT_BRACKET):
+            return None
+        node = parser.expectRule(Expression)
+        parser.expect(Token.RIGHT_BRACKET)
+        return ListIndexing([node])
+
+class Terminal(Rule):
+    @staticmethod
+    def match(parser):
+        accepted = [Token.BOOLEAN,Token.MACRO,Token.IDENTIFIER,Token.INTEGER,Token.STRING,Token.FLOATING]
+        for token_type in accepted:
+            if parser.accept(token_type):
+                return Terminal([parser.current])
+        return None
+
     
-test_code = "#include <hej.test>\n#requireadmin\n"
+class Expression(Rule):
+    @staticmethod
+    def match(parser):
+        nodes = []
+        if parser.acceptRule(UnaryOperator):
+            nodes.append(parser.matched_rule)
+            nodes.append(parser.expectRule(Factor))
+        elif parser.acceptRule(Factor):
+            nodes.append(parser.matched_rule)
+        else:
+            return None
+        if parser.acceptRule(BinaryOperator):
+            nodes.append(parser.matched_rule)
+            nodes.append(parser.expectRule(Expression))
+        return Expression(nodes)
+            
+
+class Program(Rule):
+    @staticmethod
+    def match(parser):
+        nodes = []
+        while parser.acceptAnyRule([Include,Directive]):
+            nodes.append(parser.matched_rule)
+        if parser.acceptRule(Block):
+            nodes.append(parser.matched_rule)
+        parser.expect(Token.EOF)
+        return Program(nodes)
+        
+   
+    
+test_code = """
+if True or False then
+123
+if "derp" then
+"herp"
+elseif "hihi" then
+"kebab"
+endif
+else
+321
+endif
+"""
 parser = Parser(lexer.lex_string(test_code))
 
 
 p = parser.acceptRule(Program)
-print(p.directives)
+print(p)
 
     
     
--- test_parser.py
+++ test_parser.py
@@ -7,12 +7,13 @@ from rd_parser import *
 
 def match_rule(code,rule):
     p = Parser(lex_string(code))
-    return p.expectRule(p)
+    return p.expectRule(rule)
 
 class TestParser(unittest.TestCase):
     def test_accept_token(self):
         self.assertTrue(Parser(lex_string("123")).accept(Token.INTEGER))
         self.assertTrue(Parser(lex_string("123")).accept(Token.INTEGER,123))
+        self.assertFalse(Parser(lex_string("123")).accept(Token.INTEGER,321))
         self.assertFalse(Parser(lex_string("123.45")).accept(Token.INTEGER))
     def test_current_token(self):
         p = Parser(lex_string("123 456"))
@@ -20,7 +21,55 @@ class TestParser(unittest.TestCase):
         self.assertEqual(p.current.value,123)
         p.accept(Token.STRING)
         self.assertEqual(p.current.value,123)
+    def test_expect_token(self):
+        self.assertTrue(Parser(lex_string("123")).expect(Token.INTEGER))
+        self.assertTrue(Parser(lex_string("123")).expect(Token.INTEGER,123))
+        self.assertRaises(Exception, Parser(lex_string("123")).expect,Token.IDENTIFIER,321)
+        self.assertRaises(Exception, Parser(lex_string("123")).expect,Token.IDENTIFIER)
     def test_accept_rule(self):
         self.assertTrue(Parser(lex_string("#include-once")).acceptRule(Directive))
         self.assertFalse(Parser(lex_string("#include-once")).acceptRule(Function))
     def test_expect_rule(self):
+        self.assertTrue(Parser(lex_string("#include-once")).expectRule(Directive))
+        self.assertRaises(Exception, Parser(lex_string("#include-once")).expectRule,Function)
+    def test_accept_multiple_tokens(self):
+        p = Parser(lex_string("1 2 3 4"))
+        p.accept_multiple_tokens([[Token.INTEGER],[Token.FLOATING]])
+        self.assertEqual(p.current,None)
+        
+        p.accept(Token.INTEGER)
+        p.accept_multiple_tokens([[Token.INTEGER],[Token.INTEGER],[Token.INTEGER,666]])
+        self.assertEqual(p.current.value,1)
+        
+        matched = p.accept_multiple_tokens([[Token.INTEGER],[Token.INTEGER,3],[Token.INTEGER]])
+        self.assertEqual([x.value for x in matched],[2,3,4])
+        
+        
+        
+
+
+
+
+class TestIncludeRule(unittest.TestCase):
+    def test_bracket_match(self):
+        i = match_rule("#include <file.ext>",Include)
+        self.assertEqual(i.token.value,"file.ext")
+    def test_quote_match(self):
+        i = match_rule('#include "file.ext"',Include)
+        self.assertEqual(i.token.value,"file.ext")
+    def test_non_match(self):
+        self.assertRaises(Exception,match_rule,"#include",Include)
+        
+class TestFunctionRule(unittest.TestCase):
+    def test_empty_function(self):
+        self.assertTrue(match_rule("func a()\nendfunc",Function))
+    def test_non_empty_function(self):
+        self.assertTrue(match_rule("func a()\n12+5-5\nendfunc",Function))
+    def test_function_name(self):
+        self.assertEqual(match_rule("func abc()\nendfunc",Function).name.value,"ABC")
+    def test_single_argument(self):
+        self.assertEqual(match_rule("func a(b)\nendfunc",Function).arguments[0].value,"B")
+    def test_multiple_arguments(self):
+        self.assertEqual([x.value for x in match_rule("func a(b,c,d)\nendfunc",Function).arguments],["B","C","D"])
+        
+        
