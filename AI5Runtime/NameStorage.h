2d20e631d01d4ec1186bd1a78429a899243a79ed
Andreas <Andreas@chase.andhen.mine.nu>
2013-05-27 07:53:58 +0200

    Fixed memory leak in scope class.

--- AI5Runtime/NameStorage.h
+++ AI5Runtime/NameStorage.h
@@ -91,16 +91,31 @@ public:
 		return nameObj;
 	}
 
+	// Make the scope ready for reusing.
 	void reset()
 	{
+		// Clear all names in the lookup. Will release variants. 
 		for(auto it=lookup.begin();it!=lookup.end();it++)
 		{
 			it->second->clear();
+
+			// Is there room left in the name pool?
+			// If it is, add released name into it.
+			if(namePool.size() < POOL_SIZE)
+			{
+				namePool.push(it->second);
+			}
+			else
+			{
+				delete it->second;
+			}
 		}
 
+		// Make sure the indexTable is all null's.
 		for(int i=0;i<usedIndexes.size();i++)
 		{
-			
+/*			// Is there room left in the name pool?
+			// If it is, add released name into it.
 			if(namePool.size() < POOL_SIZE)
 			{
 				namePool.push(indexTable[usedIndexes[i]]);
@@ -109,15 +124,27 @@ public:
 			{
 				delete indexTable[usedIndexes[i]];
 			}
+			*/
 			indexTable[usedIndexes[i]] = nullptr;
+			
 		}
 
 		lookup.clear();
 		usedIndexes.clear();
 	}
 private:
+	// The string->name lookup.
+	// All names in the scope NEEDS to be in this map.
 	std::map<std::wstring,Name*> lookup;
+
+	// Used to provide super fast lookup of names in this scope.
+	// Not all names are necessarily in this table. 
 	std::vector<Name*> indexTable;
+
+	// List of used index in this table.
+	// Used to avoid clearing the entire indexTable whenever a scope object is reused.
 	std::vector<int> usedIndexes;
+
+	// Keep a pool of names to avoid extensive heap alloc.
 	FastStack<Name*> namePool;
 };
