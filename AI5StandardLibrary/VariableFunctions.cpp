9153bc81dec0f098bf7570b94d1fb64750937be1
Andreas Karlsson <andreas.karlsson3@gmail.com>
2013-06-24 21:10:31 +0200

    Added some Variable functions.

--- AI5StandardLibrary/VariableFunctions.cpp
+++ AI5StandardLibrary/VariableFunctions.cpp
@@ -2,6 +2,7 @@
 #include "..\AI5Runtime\StringVariant.h"
 #include "..\AI5Runtime\Integer32Variant.h"
 #include "..\AI5Runtime\BinaryVariant.h"
+#include "..\AI5Runtime\BooleanVariant.h"
 #include "..\AI5Runtime\StackMachine.h"
 #include <string>
 #include <wchar.h>
@@ -26,6 +27,12 @@ void VariableFunctions::registerFunctions(StackMachine* machine)
 
 	machine->addBuiltInFunction(L"stringtobinary",std::bind(&stringToBinary,instance,_1,_2));
 	machine->addBuiltInFunction(L"binaryToString",std::bind(&binaryToString,instance,_1,_2));
+	machine->addBuiltInFunction(L"isfloat",std::bind(&isFloat,instance,_1,_2));
+	machine->addBuiltInFunction(L"isstring",std::bind(&isString,instance,_1,_2));
+	machine->addBuiltInFunction(L"isarray",std::bind(&isArray,instance,_1,_2));
+	machine->addBuiltInFunction(L"ishashmap",std::bind(&isHashMap,instance,_1,_2));
+	machine->addBuiltInFunction(L"isbool",std::bind(&isBool,instance,_1,_2));
+	machine->addBuiltInFunction(L"string",std::bind(&isString,instance,_1,_2));
 }
 
 
@@ -60,4 +67,42 @@ Variant* VariableFunctions::stringToBinary(Variant** args,int argCount)
 Variant* VariableFunctions::binaryToString(Variant** args,int argCount)
 {
 	return nullptr;
+}
+
+
+
+Variant* VariableFunctions::isFloat(Variant** args,int argCount)
+{
+	validateArgCount(argCount,1,1);
+	return BooleanVariant::Get(args[0]->isFloatingType(),true);
+	
+}
+Variant* VariableFunctions::isString(Variant** args,int argCount)
+{
+	validateArgCount(argCount,1,1);
+	return BooleanVariant::Get(args[0]->isStringType(),true);
+	
+}
+Variant* VariableFunctions::isArray(Variant** args,int argCount)
+{
+	validateArgCount(argCount,1,1);
+	return BooleanVariant::Get(args[0]->isListType(),true);
+	
+}
+Variant* VariableFunctions::isHashMap(Variant** args,int argCount)
+{
+	validateArgCount(argCount,1,1);
+	return BooleanVariant::Get(args[0]->isHashMap(),true);
+	
+}
+Variant* VariableFunctions::isBool(Variant** args,int argCount)
+{
+	validateArgCount(argCount,1,1);
+	return BooleanVariant::Get(args[0]->isBooleanType(),true);
+	
+}
+Variant* VariableFunctions::ToString(Variant** args,int argCount)
+{
+	validateArgCount(argCount,1,1);
+	return new StringVariant(args[0]->toString());	
 }
--- AI5StandardLibrary/VariableFunctions.h
+++ AI5StandardLibrary/VariableFunctions.h
@@ -12,5 +12,11 @@ private:
 	VariableFunctions(void);
 	Variant* stringToBinary(Variant** args,int argCount);
 	Variant* binaryToString(Variant** args,int argCount);
+	Variant* isFloat(Variant** args,int argCount);
+	Variant* isString(Variant** args,int argCount);
+	Variant* isArray(Variant** args,int argCount);
+	Variant* isHashMap(Variant** args,int argCount);
+	Variant* isBool(Variant** args,int argCount);
+	Variant* ToString(Variant** args,int argCount);
 };
 
