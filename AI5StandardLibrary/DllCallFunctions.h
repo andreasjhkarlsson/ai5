50ddfa0a47c06be5b02896b6af51d247b8776f28
dragana_r <devnull@localhost>
2013-06-25 21:38:21 +0200

    Added dllcall and dllcalladdress.

---
+++ AI5StandardLibrary/DllCallFunctions.h
@@ -0,0 +1,17 @@
+#pragma once
+#include "functions.h"
+
+class Variant;
+class StackMachine;
+
+class DllCallFunctions : public Functions
+{
+public:
+	static void registerFunctions(StackMachine* machine);
+	~DllCallFunctions();
+private:
+	DllCallFunctions();
+	Variant* dllcall(Variant** args,int argsSize);
+	Variant* dllcalladdress(Variant** args,int argsSize);
+};
+
