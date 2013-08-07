eac29107abf717ea22de477f09b0e500b1bbb6b2
Andreas Karlsson <andreas.karlsson3@gmail.com>
2013-08-07 22:32:23 +0200

    Deploy-script updated.

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,8 @@
 cd AI5Compiler
 python build_exe.py build
 copy build\exe.win32-3.3\* ..\wscite\bin\
+copy ai5-include\* ..\wscite\bin\ai5-include\
 cd ..
 copy AI5Runtime\Release\AI5Runtime.exe wscite\bin\ai5r.exe
+copy AI5Runtime\Release\*.dll wscite\bin\
 pause
