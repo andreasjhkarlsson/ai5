2c0b4a96f345cbae99006555c4d45c90cd7b2ea6
Andreas Karlsson <andreas.karlsson3@gmail.com>
2013-08-09 16:27:30 +0200

    Fixed runtime build script for x86 platforms.

--- AI5Runtime/build_release.bat
+++ AI5Runtime/build_release.bat
@@ -1,2 +1,3 @@
-call "%programfiles(x86)%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+if not "%programfiles(x86)%"=="" call "%programfiles(x86)%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
+if "%programfiles(x86)%"=="" call "%programfiles%\Microsoft Visual Studio 11.0\VC\vcvarsall.bat" x86
 msbuild AI5Runtime.sln /t:Build /p:Configuration=Release;Platform=Win32
